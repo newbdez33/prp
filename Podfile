1d6c000fb659601c9bef61cf0e9458e3d7f324ee
Jacky <newbdez33@gmail.com>
2017-04-03 20:15:08 +0900

    group profile and cocoapods

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+target 'pr' do
+	pod 'AsyncDisplayKit'
+    pod 'RxSwift', '~> 3.0'
+end
---
+++ Pods/Target Support Files/Pods-pr/Pods-pr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pr : NSObject
+@end
+@implementation PodsDummy_Pods_pr
+@end
