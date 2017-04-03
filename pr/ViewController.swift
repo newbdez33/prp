dac967e07e4e9f0a7d94de70b0f7d3bdbcf1fa3d
Jacky <newbdez33@gmail.com>
2017-04-03 19:45:28 +0900

    pr app project

--- pr/ViewController.swift
+++ pr/ViewController.swift
@@ -12,7 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        print(documentsPath);
     }
 
     override func didReceiveMemoryWarning() {
