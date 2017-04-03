441ef0903706f24b33d844e3789eb077e1d56044
Jacky <newbdez33@gmail.com>
2017-04-03 21:28:28 +0900

    app icon added

--- urlaction/ActionViewController.swift
+++ urlaction/ActionViewController.swift
@@ -11,48 +11,27 @@ import MobileCoreServices
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var testLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
         let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
         print(documentsPath);
-        // Get the item[s] we're handling from the extension context.
-        
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
+
         for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
             for provider in item.attachments! as! [NSItemProvider] {
-                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
-                        OperationQueue.main.addOperation {
-                            if let strongImageView = weakImageView {
-                                if let imageURL = imageURL as? URL {
-                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-                                }
-                            }
-                        }
+                if provider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
+                    provider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil, completionHandler: { (coding:NSSecureCoding?, error:Error!) in
+                        let url = coding as! NSURL
+                        self.testLabel.text = url.absoluteString
                     })
                     
-                    imageFound = true
                     break
                 }
             }
             
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
-            }
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func done() {
