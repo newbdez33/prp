aac33a4f2c94c563e6f3052be5234cddd4b2a44c
Jacky <newbdez33@gmail.com>
2017-04-30 21:27:42 +0900

    setting layout staging

---
+++ pr/controller/HowtoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HowtoViewController.swift
+//  pr
+//
+//  Created by JackyZ on 2017/04/30.
+//  Copyright © 2017 Salmonapps. All rights reserved.
+//
+
+import UIKit
+
+class HowtoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.white
+    }
+
+}
--- pr/controller/SettingsViewController.swift
+++ pr/controller/SettingsViewController.swift
@@ -15,6 +15,10 @@ class SettingsViewController: FormViewController {
         super.viewDidLoad()
         title = "Settings"
         form +++
+            Section() {
+                $0.header = HeaderFooterView<PRLogoView>(.class)
+            }
+            +++
             Section("PRICE DROP NOTIFICATION")
             <<<
             SwitchRow("Price drop notification") {
@@ -26,28 +30,79 @@ class SettingsViewController: FormViewController {
             }
             +++
             Section("USE PRICE BOT")
-            <<< ButtonRow("How to use it") { row in
-                row.title = row.tag
+            <<< ButtonRow("How to use it") {
+                $0.title = $0.tag
+                $0.cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 15)!
+                $0.cell.textLabel?.textColor = UIColor.prBlack()
+                $0.presentationMode = .show(controllerProvider: ControllerProvider.callback {
+                    return HowtoViewController()
+                }, onDismiss: { vc in _ = vc.navigationController?.popViewController(animated: true) })
             }
-            <<< ButtonRow("URL Schemes") { row in
-                row.title = row.tag
+            <<< ButtonRow("URL Schemes") {
+                $0.title = $0.tag
+                $0.cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 15)!
+                $0.cell.textLabel?.textColor = UIColor.prBlack()
+                $0.presentationMode = .show(controllerProvider: ControllerProvider.callback {
+                    return URLSchemesViewController()
+                }, onDismiss: { vc in _ = vc.navigationController?.popViewController(animated: true) })
             }
             +++
             Section("MISC")
-            <<< ButtonRow("Recommend Price Bot") { row in
-                row.title = row.tag
+            <<< ButtonRow("Recommend Price Bot") {
+                $0.title = $0.tag
+                $0.cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 15)!
+                $0.cell.tintColor = UIColor.prBlack()
             }
-            <<< ButtonRow("Rate on App Store") { row in
-                row.title = row.tag
+            <<< ButtonRow("Rate on App Store") {
+                $0.title = $0.tag
+                $0.cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 15)!
+                $0.cell.tintColor = UIColor.prBlack()
             }
-            <<< ButtonRow("Give us feedback") { row in
-                row.title = row.tag
+            <<< ButtonRow("Give us feedback") {
+                $0.title = $0.tag
+                $0.cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 15)!
+                $0.cell.tintColor = UIColor.prBlack()
             }
-            <<< ButtonRow("Visit our website") { row in
-                row.title = row.tag
+            <<< ButtonRow("Visit our website") {
+                $0.title = $0.tag
+                $0.cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 15)!
+                $0.cell.tintColor = UIColor.prBlack()
             }
             +++
-            Section(footer: "Version 0.1(1)")
+            Section() { section in
+                section.header = {
+                    var header = HeaderFooterView<UIView>(.callback({
+                        let view = UILabel(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+                        let short = Bundle.main.infoDictionary!["CFBundleShortVersionString"]
+                        let bundle = Bundle.main.infoDictionary!["CFBundleVersion"]
+                        view.text = "Price Bot Version \(short!) (build \(bundle!))"
+                        view.font = UIFont(name: "HelveticaNeue-Light", size: 12)!
+                        view.textColor = UIColor.prGray()
+                        view.textAlignment = .center
+                        view.backgroundColor = .clear
+                        return view
+                    }))
+                    header.height = { 50 }
+                    return header
+                }()
+        }
     }
 
+}
+
+class PRLogoView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let imageView = UIImageView(image: UIImage(named: "pr-banner"))
+        imageView.frame = CGRect(x: 0, y: 0, width: 320, height: 90)
+        imageView.autoresizingMask = .flexibleWidth
+        self.frame = CGRect(x: 0, y: 0, width: 320, height: 90)
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
