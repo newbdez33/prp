56595e1402ca303c63a779b52a2da81ce7582bdd
Jacky <newbdez33@gmail.com>
2017-05-17 23:18:52 +0900

    chinese support

--- pr/controller/UsagePages/Usage2ViewController.swift
+++ pr/controller/UsagePages/Usage2ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class Usage2ViewController: UIViewController {
     
+    @IBOutlet weak var openButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Everything is here! Open Amazon site now
+        let attrString = NSMutableAttributedString(string: NSLocalizedString("Everything is here!", comment: ""),
+                                                   attributes: [ NSFontAttributeName: UIFont(name:"HelveticaNeue-Light", size:14)! ])
+        
+        attrString.append(NSMutableAttributedString(string: NSLocalizedString("Open Amazon site now", comment: ""),
+                                                    attributes: [NSFontAttributeName: UIFont(name:"HelveticaNeue-Bold", size:14)! ]))
+        openButton.setAttributedTitle(attrString, for: .normal)
     }
     
     
     @IBAction func openAction(_ sender: UIButton) {
-        let alert = UIAlertController(title: "We are currently support those sites.", message: nil, preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: NSLocalizedString("We are currently support those sites.", comment: ""), message: nil, preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "amazon.com", style: .default, handler: { (_) in
             UIApplication.shared.open(URL(string:"http://amazon.com")!, options: [:], completionHandler: nil)
         }))
@@ -31,7 +38,7 @@ class Usage2ViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "amazon.de", style: .default, handler: { (_) in
             UIApplication.shared.open(URL(string:"http://amazon.de")!, options: [:], completionHandler: nil)
         }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: nil))
         present(alert, animated: true, completion: nil)
     }
 
